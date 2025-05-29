ccdc37c36b0cb403c5c67673dc521072e22f9ee5
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-05-29 17:34:01 +0300

    Refactor : Responsive design has been implemented

--- src/components/FooterLogos.jsx
+++ src/components/FooterLogos.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function FooterLogos() {
     return (
-        <div className="w-full bg-white py-7 flex justify-center gap-20 border-t border-gray-100">
+        <div className="w-full bg-white py-7  justify-center gap-20 border-t border-gray-100 hidden sm:flex">
             <img src="/logo/logo1.png" alt="logo1" className="h-15" />
             <img src="/logo/logo3.png" alt="logo3" className="h-15 " />
             <img src="/logo/logo4.png" alt="logo4" className="h-15" />
